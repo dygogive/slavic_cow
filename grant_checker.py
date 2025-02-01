3eeb16911fb0d79c572dd480c917b8f15abade4e
Roman <38737614+dygogive@users.noreply.github.com>
2025-02-01 23:57:40 +0200

    Update grant_checker.py

--- grant_checker.py
+++ grant_checker.py
@@ -1,12 +1,15 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 @app.route("/")
 def home():
     return "Бот працює!"
 
-@app.route(f"/<TELEGRAM_BOT_TOKEN>", methods=["POST"])
+@app.route(f"/{TELEGRAM_BOT_TOKEN}", methods=["POST"])
 def telegram_webhook():
     update = request.get_json()
     if "message" in update:
@@ -14,3 +17,6 @@ def telegram_webhook():
         if text == "/start":
             print("Команда /start отримана!")
     return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
