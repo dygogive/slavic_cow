382d19b5825fa6e091dcec85d4ae7333ed1fc655
Roman <38737614+dygogive@users.noreply.github.com>
2025-02-01 23:47:41 +0200

    Update grant_checker.py

--- grant_checker.py
+++ grant_checker.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import os
 import requests
 
+app = Flask(__name__)  # ВАЖЛИВО: Змінна "app" повинна бути доступна
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 BASE_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
@@ -10,15 +11,6 @@ WEBHOOK_URL = f"https://slaviccow-production.up.railway.app/{TELEGRAM_BOT_TOKEN}
 def set_webhook():
     response = requests.post(f"{BASE_URL}/setWebhook", data={"url": WEBHOOK_URL})
     print(f"Webhook set: {response.status_code}, {response.json()}")
-
-if __name__ == "__main__":
-    set_webhook()
-
-
-app = Flask(__name__)
-
-# Токен Telegram бота
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 @app.route(f"/{TELEGRAM_BOT_TOKEN}", methods=["POST"])
 def telegram_webhook():
@@ -32,6 +24,6 @@ def telegram_webhook():
     
     return "OK", 200
 
-
 if __name__ == "__main__":
+    set_webhook()
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
